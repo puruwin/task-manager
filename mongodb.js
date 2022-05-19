074a0b0c706e8943d343437db1528bc2c8a68ac6
David Pérez Martínez <taborlindp@gmail.com>
2022-05-19 16:41:01 +0200

    The Object ID

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,17 @@
 // CRUD create-read-update-delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -14,8 +21,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName)
 
     // db.collection('users').insertOne({
-    //     name: 'David',
-    //     age: 22
+    //     name: 'Vikram',
+    //     age: 26
     // }, (error, result) => {
     //     if (error) {
     //         return console.log('Unable to insert user')
@@ -41,22 +48,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     console.log(result.ops)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Clean the house',
-            completed: true
-        }, {
-            description: 'Renew inspection',
-            completed: false
-        }, {
-            description: 'Pot plants',
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert documents!')
-        }
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Clean the house',
+    //         completed: true
+    //     }, {
+    //         description: 'Renew inspection',
+    //         completed: false
+    //     }, {
+    //         description: 'Pot plants',
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
 
-        console.log(result.ops)
-    })
+    //     console.log(result.ops)
+    // })
+
+
 })
