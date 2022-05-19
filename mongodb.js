466594908713cca9517e3280b2398b027ad3cca4
David Pérez Martínez <taborlindp@gmail.com>
2022-05-19 16:14:47 +0200

    Inserting documents

--- mongodb.js
+++ mongodb.js
@@ -8,12 +8,55 @@ const databaseName = 'task-manager'
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
-       return console.log('Unable to connect to database!')
+        return console.log('Unable to connect to database!')
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'David',
-        age: 22
+
+    // db.collection('users').insertOne({
+    //     name: 'David',
+    //     age: 22
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Tamar',
+    //         age: 21
+    //     },
+    //     {
+    //         name: 'Jorge',
+    //         age: 21
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean the house',
+            completed: true
+        }, {
+            description: 'Renew inspection',
+            completed: false
+        }, {
+            description: 'Pot plants',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents!')
+        }
+
+        console.log(result.ops)
     })
 })
