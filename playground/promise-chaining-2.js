cd7a5d6f7f2eea0c4e43d860e874da56ad27b085
David Pérez Martínez <taborlindp@gmail.com>
2022-05-20 14:30:54 +0200

    Async/await II

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('62874ce560ef06268034d975').then((task) => {
-    console.log(task)
-    return Task.countDocuments({})
-}).then((tasksCount) => {
-    console.log(tasksCount)
+// Task.findByIdAndDelete('62874ce560ef06268034d975').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({})
+// }).then((tasksCount) => {
+//     console.log(tasksCount)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const delTask = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('6286835a1ec43543f86a0106').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
